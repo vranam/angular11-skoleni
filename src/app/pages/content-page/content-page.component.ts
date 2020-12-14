c1c1b3bd47a7efcdb70f1d0ebf73e22d78b109cd
Miroslav Vrana <mirek.vrana@gmail.com>
2020-12-14 15:42:23 +0100

    structural directives

--- src/app/pages/content-page/content-page.component.ts
+++ src/app/pages/content-page/content-page.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentPageComponent implements OnInit {
 
-  constructor() { }
+  data: Article[] = [
+    {title: 'Článek 1', content: 'blabla', author: 'Jan Nevidím', created: new Date()},
+    {title: 'Článek 2', content: 'vldsdsjdj jddshxs jdjsjjdjd jsdh', author: 'xyz', created: new Date()}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+
+interface Article {
+  title: string;
+  content: string;
+  author: string;
+  created: Date;
 }
