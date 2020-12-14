7e0020fd1e72fc08b8118d1832c0f061a71a3d24
Miroslav Vrana <mirek.vrana@gmail.com>
2020-12-14 12:07:57 +0100

    registrastion-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './components/registration-form/registration-form.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-skoleni-app';
+  pepa: User;
+
+  constructor() {
+    this.pepa = {
+      username: 'user1',
+      forename: 'Pepa',
+      surname: 'Novák'
+    };
+  }
+
+  addUser(value: User): void {
+    console.log('new user', value);
+    this.pepa = value;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {RegistrationFormComponent} from './components/registration-form/registration-form.component';
 import {ExampleServiceService} from './service/example-service.service';
 import { NumbersPipe } from './pipes/numbers.pipe';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationFormComponent,
-    NumbersPipe
+    NumbersPipe,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule
